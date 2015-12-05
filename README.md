fdd1bbfccc353a6e284641c580f5493274b5b24c
Michael Dreiling <michaeldreiling@localprojects.net>
2015-12-05 14:41:51 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,30 @@
+# Crawl
+
+A command line script to crawl websites.
+
+*Usage*
+
+    crawl --help
+
+## Requirements
+
+- python 2.7 or >3.3
+- qt
+
+## Install
+
+    brew install qt
+    git clone https://github.com/local-projects/crawl
+    cd crawl
+    python setup.py install --record uninstall.txt
+
+## Uninstall
+
+    cat uninstall.txt | xargs rm -rf
+
+## TODO:
+
+- Graph visualization of site (a visual site map)
+- Full backup of site (downloads all html/css/js/images/video. Ie, generates a static version of the site)
+- Multiple threads with user-specified amount (defaulting to 1)
+- Better ajax loading detection (can currently just wait a set time per page)
---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from setuptools import setup
+import sys
+
+version = sys.version_info[:2]
+if version < (2, 7):
+    print('crawl requires Python version 2.7 or later' +
+          ' ({}.{} detected).'.format(*version))
+    sys.exit(-1)
+elif (3, 0) < version < (3, 3):
+    print('crawl requires Python version 3.3 or later' +
+          ' ({}.{} detected).'.format(*version))
+    sys.exit(-1)
+
+VERSION = '0.1'
+
+install_requires = ['dryscrape', 'docopt', 'reppy']
+
+setup(
+    name='crawl',
+    version=VERSION,
+    description="A not-so-magnificent website crawler.",
+    author='Michael Dreiling',
+    author_email='michaeldreiling@localprojects.net',
+    url='https://github.com/local-projects/crawl',
+    license='MIT',
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    scripts=['crawl.py'],
+)
